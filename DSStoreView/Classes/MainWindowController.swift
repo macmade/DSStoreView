ed4a22b4f6b82488f7ccc69693bdc10958f215cf
macmade <macmade@xs-labs.com>
2021-08-04 12:56:53 +0200

    Preparing for the main window.

--- DSStoreView/Classes/MainWindowController.swift
+++ DSStoreView/Classes/MainWindowController.swift
@@ -1,18 +1,18 @@
 /*******************************************************************************
  * The MIT License (MIT)
- *
+ * 
  * Copyright (c) 2021 Jean-David Gadina - www.xs-labs.com
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,19 +24,15 @@
 
 import Cocoa
 
-@main class ApplicationDelegate: NSObject, NSApplicationDelegate
+public class MainWindowController: NSWindowController
 {
-    @objc public private( set ) dynamic var mainWindowController = MainWindowController()
-    
-    func applicationDidFinishLaunching( _ notification: Notification )
+    public override var windowNibName: NSNib.Name?
     {
-        self.mainWindowController.window?.layoutIfNeeded()
-        self.mainWindowController.window?.center()
-        self.mainWindowController.window?.makeKeyAndOrderFront( nil )
+        "MainWindowController"
     }
     
-    func applicationShouldTerminateAfterLastWindowClosed( _ sender: NSApplication ) -> Bool
+    public override func windowDidLoad()
     {
-        true
+        super.windowDidLoad()
     }
 }
